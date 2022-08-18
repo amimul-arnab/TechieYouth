9ad82931bd37609955f12ec472e145181cb888cb
Its-Sturdy <102828836+Its-Sturdy@users.noreply.github.com>
2022-08-18 13:08:35 -0400

    Techie Youth Website
    I created this website for the Web Development unit with Techie Youth. This is the first time creating a website using HTML, CSS and Javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+function check() {
+    var name = document.getElementById("username");
+    var date = document.getElementById("today");
+    var text = document.getElementById("message")
+
+    document.writeln(str(name) + "\n" + str(date) + "\n" + str(text))
+}
